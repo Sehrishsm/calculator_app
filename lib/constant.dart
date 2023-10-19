cdf5e16b9a64de287a5835a565019153e818db83
Sehrishsm <sehrishmoujahid967@gmail.com>
2023-10-19 18:41:35 +0500

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+const Color  whiteColor = Colors.white;
+const Color grayColor = Colors.green;
+const buttonTextStyle = TextStyle(color: Colors.white);
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:calculator_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  print('hello world');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
